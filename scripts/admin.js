e4e138af81cf9627d9d217849cf1b9e4459f9158
gast-exe <gast228@bigmir.net>
2023-01-01 15:36:42 +0200

    Fix bad code

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,60 +1,58 @@
-let adminForm = document.getElementById("admin-form");
-
-adminForm.addEventListener("submit", function (event) {
-    event.preventDefault();
-    let data = JSON.stringify({
-        "name": event.target["name"].value,
-        "description": event.target["desc"].value,
-        "price": event.target["price"].value,
-        "photo_url": event.target["photo_url"].value
-    });
-    let xhr = new XMLHttpRequest();
-    xhr.open("POST", "https://marketdb-46b0.restdb.io/rest/products");
-    xhr.setRequestHeader("content-type", "application/json");
-    xhr.setRequestHeader("x-apikey", "63b00b13969f06502871a7ee"); // 63b00b13969f06502871a7ee
-    xhr.setRequestHeader("cache-control", "no-cache");
-    xhr.send(data);
-
-    xhr.onload = function () {
-        window.open("index.html", "_self");
-    };
-});
-
-let orderCards = document.getElementById("order-cards");
-let xhr = new XMLHttpRequest();
-xhr.open("GET", "https://marketdb-46b0.restdb.io/rest/order");
-xhr.setRequestHeader("content-type", "application/json");
-xhr.setRequestHeader("x-apikey", "63b00b13969f06502871a7ee");
-xhr.setRequestHeader("cache-control", "no-cache");
-xhr.responseType = "json";
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://marketdb-46b0.restdb.io/rest/order');
+xhr.setRequestHeader('content-type', 'application/json');
+xhr.setRequestHeader('x-apikey', '63b00b13969f06502871a7ee');
+xhr.setRequestHeader('cache-control', 'no-cache');
+xhr.responseType = 'json';
 xhr.send();
-xhr.onload = function () {
-    let orders = xhr.response;
-    for (const order of orders) {
-        orderCards.innerHTML += `
-        <div class="card my-3">
-            <div class="card-body">
-                <h5 class="card-title">Order: ${order._id}</h5>
-                <p class="card-text">Customer name: ${order.name}</p>
-                <p class="card-text">Phone: ${order.phone}</p>
-                <p class="card-text">Address: ${order.address}</p>
-                <p class="card-text">Post office number: ${order.post_number}</p>
-                <div id="id-${order._id}"></div>
-            </div>
-        </div>
-        `;
-        let productsEl = document.getElementById("id-" + order._id);
-        let sum = 0;
-        for (const product of JSON.parse(order.products)) {
-            productsEl.innerHTML += `
-                <p>
-                    <img width="100px" src="${product.photo_url}">
-                    ${product.name} |${product.price}UAH
-                    <hr>
-                </p>
-            `;
-            sum += +product.price;
-        }
-        productsEl.innerHTML += `<p>Total price: <b>${sum}UAH</b>`;
-    }
+xhr.onload = () => {
+	const orders = xhr.response;
+	for (const order of orders) {
+		document.getElementById('order-cards').innerHTML += `
+		<div class="card my-3">
+			<div class="card-body">
+				<h5 class="card-title">Order: ${order._id}</h5>
+				<p class="card-text">Customer name: ${order.name}</p>
+				<p class="card-text">Phone: ${order.phone}</p>
+				<p class="card-text">Address: ${order.address}</p>
+				<p class="card-text">Post office number: ${order.post_number}</p>
+				<div id="id-${order._id}"></div>
+			</div>
+		</div>
+		`;
+		
+		const productsEl = document.getElementById('id-' + order._id);
+		let sum = 0;
+		for (const product of JSON.parse(order.products)) {
+			productsEl.innerHTML += `
+			<p>
+				<img width="100px" src="${product.photo_url}">
+				${product.name} |${product.price}UAH
+				<hr>
+			</p>
+			`;
+			sum += +product.price;
+		}
+		productsEl.innerHTML += `<p>Total price: <b>${sum}UAH</b>`;
+	}
 };
+
+document.getElementById('admin-form').addEventListener('submit', event => {
+	event.preventDefault();
+	const data = JSON.stringify({
+		'name': event.target['name'].value,
+		'description': event.target['desc'].value,
+		'price': event.target['price'].value,
+		'photo_url': event.target['photo_url'].value
+	});
+	const xhr = new XMLHttpRequest();
+	xhr.open('POST', 'https://marketdb-46b0.restdb.io/rest/products');
+	xhr.setRequestHeader('content-type', 'application/json');
+	xhr.setRequestHeader('x-apikey', '63b00b13969f06502871a7ee');
+	xhr.setRequestHeader('cache-control', 'no-cache');
+	xhr.send(data);
+
+	xhr.onload = () => {
+		window.open('index.html', '_self');
+	};
+});
--- scripts/script.js
+++ scripts/script.js
@@ -1,117 +1,112 @@
-let productsGrid = document.getElementById("products-grid");
-let productsArray = [];
+const cartProd = document.getElementById('cart-products');
 
-let xhr = new XMLHttpRequest();
-xhr.open("GET", "https://marketdb-46b0.restdb.io/rest/products");
-xhr.setRequestHeader("content-type", "application/json");
-xhr.setRequestHeader("x-apikey", "63b00b13969f06502871a7ee");
-xhr.setRequestHeader("cache-control", "no-cache");
+let productsArray;
+let cart = [];
+
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'https://marketdb-46b0.restdb.io/rest/products');
+xhr.setRequestHeader('content-type', 'application/json');
+xhr.setRequestHeader('x-apikey', '63b00b13969f06502871a7ee');
+xhr.setRequestHeader('cache-control', 'no-cache');
 xhr.send();
-xhr.onload = function () {
-	let products = JSON.parse(xhr.response);
-	productsArray = products;
+xhr.onload = () => {
+	const productsGrid = document.getElementById('products-grid');
+	productsArray = JSON.parse(xhr.response);
+
 	productsGrid.innerHTML = null;
-	for (const product of products) {
-		let productElement = document.createElement("div");
-		productElement.classList.add("product");
+	for (const product of productsArray) {
+		const productElement = document.createElement('div');
+		productElement.classList.add('product');
 		productElement.innerHTML = `
-            <h2 class="product-name">${product.name}</h2>
-            <img class="product-img" src="${product.photo_url}" alt="${product.name}">
-            <p class="product-desc">${product.description}</p>
-            <p class="product-price">Price: ${product.price}UAH</p>
-            <button onclick="addProductToCart('${product._id}')">Buy</button>
-        `;
+				<h2 class="product-name">${product.name}</h2>
+				<img class="product-img" src="${product.photo_url}" alt="${product.name}">
+				<p class="product-desc">${product.description}</p>
+				<p class="product-price">Price: ${product.price}UAH</p>
+				<button onclick="addProductToCart('${product._id}')">Buy</button>
+		  `;
 		productsGrid.append(productElement);
 	}
 };
 
-let cartProd = document.getElementById("cart-products");
-
-function openCart() {
-	cartProd.classList.toggle("hide");
-}
-
-let cart = [];
-
-if (localStorage.getItem("cart")) {
-	cart = JSON.parse(localStorage.getItem("cart"));
+if (localStorage.getItem('cart')) {
+	cart = JSON.parse(localStorage.getItem('cart'));
 	showCartProducts();
 }
 
-function addProductToCart(id) {
-	let product = productsArray.find(function (p) {
-		return p._id == id;
+document.getElementById('order-form').addEventListener('submit', event => {
+	event.preventDefault();
+	const data = JSON.stringify({
+		'name': event.target['name'].value,
+		'address': event.target['address'].value,
+		'phone': event.target['phone'].value,
+		'post_number': event.target['post_number'].value,
+		'products': localStorage.getItem('cart')
 	});
+
+	const xhr = new XMLHttpRequest();
+	xhr.open('POST', 'https://marketdb-46b0.restdb.io/rest/order');
+	xhr.setRequestHeader('content-type', 'application/json');
+	xhr.setRequestHeader('x-apikey', '63b00b13969f06502871a7ee');
+	xhr.setRequestHeader('cache-control', 'no-cache');
+	xhr.send(data);
+
+	xhr.onload = () => {
+		localStorage.removeItem('cart');
+		window.open('index.html', '_self');
+	};
+});
+
+function addProductToCart(id) {
+	console.log(id);
+	const cartButton = document.getElementById('cart-button');
+	const product = productsArray.find(p => p._id == id);
 	cart.push(product);
 	showCartProducts();
 
-	localStorage.setItem("cart", JSON.stringify(cart));
+	localStorage.setItem('cart', JSON.stringify(cart));
 
-	document.getElementById("cart-button").classList.add("active");
-	setTimeout(function () {
-		document.getElementById("cart-button").classList.remove("active");
-	}, 500);
+	cartButton.classList.add('active');
+	setTimeout(() => cartButton.classList.remove('active'), 500);
 }
 
 function showCartProducts() {
-	if (cart.length == 0) return cartProd.innerHTML = "Cart is empty";
+	if (cart.length == 0) return cartProd.innerHTML = 'Cart is empty';
 	cartProd.innerHTML = null;
 	let sum = 0;
 	for (const product of cart) {
 		cartProd.innerHTML += `
-            <p>
-                <img src="${product.photo_url}">
-                ${product.name} |${product.price}UAH
-                <hr>
-            </p>
-        `;
+				<p>
+					 <img src="${product.photo_url}">
+					 ${product.name} |${product.price}UAH
+					 <hr>
+				</p>
+		  `;
 		sum += +product.price;
 	}
 
 	cartProd.innerHTML += `
-        <p>Total price: <b>${sum}UAH</b></p>
-        <button onclick="buyAll(${sum})" type="button" class="btn btn-primary" 
-        data-bs-toggle="modal" data-bs-target="#exampleModal">Buy All</button>
-    `;
+		  <p>Total price: <b>${sum}UAH</b></p>
+		  <button onclick="buyAll(${sum})" type="button" class="btn btn-primary" 
+		  data-bs-toggle="modal" data-bs-target="#exampleModal">Buy All</button>
+	 `;
 }
 
-function buyAll(sum) {
-	let orderProducts = document.getElementById("order-products");
+
+const cartToggle = () => cartProd.classList.toggle('hide');
+
+const buyAll = sum => {
+	const orderProducts = document.getElementById('order-products');
+
 	orderProducts.innerHTML = null;
 	for (const product of cart) {
 		orderProducts.innerHTML += `
-            <div class="card col-6">
-                <img src="${product.photo_url}" class="card-img-top">
-                <div class="card-body">
-                    <p class="card-text">${product.name} | ${product.price}UAH</p>
-                </div>
-            </div>
-        `;
+				<div class="card col-6">
+					 <img src="${product.photo_url}" class="card-img-top">
+					 <div class="card-body">
+						  <p class="card-text">${product.name} | ${product.price}UAH</p>
+					 </div>
+				</div>
+		  `;
 	}
-	let sumElement = document.getElementById("sum");
-	sumElement.innerHTML = "Total price: " + sum + "UAH";
-}
-
-let orderForm = document.getElementById("order-form");
-
-orderForm.addEventListener("submit", function (event) {
-	event.preventDefault();
-	let data = JSON.stringify({
-		"name": event.target["name"].value,
-		"address": event.target["address"].value,
-		"phone": event.target["phone"].value,
-		"post_number": event.target["post_number"].value,
-		"products": localStorage.getItem("cart")
-	});
-	let xhr = new XMLHttpRequest();
-	xhr.open("POST", "https://marketdb-46b0.restdb.io/rest/order");
-	xhr.setRequestHeader("content-type", "application/json");
-	xhr.setRequestHeader("x-apikey", "63b00b13969f06502871a7ee");
-	xhr.setRequestHeader("cache-control", "no-cache");
-	xhr.send(data);
-
-	xhr.onload = function () {
-		localStorage.removeItem("cart");
-		window.open("index.html", "_self");
-	};
-});
+	document.getElementById('sum').innerHTML = 'Total price: ' + sum + 'UAH';
+};
